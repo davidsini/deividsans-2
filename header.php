13f0cab8a32e7a9cf2f19bc916ae29f283057298
Dēivid Sans <david.mancillas@icloud.com>
2025-11-23 22:28:16 -0600

    deleted unnecessary folders

---
+++ header.php
@@ -0,0 +1,94 @@
+<section class="bg-[#131313] md:py-[20px] relative z-50">
+    <h1 class="hidden">section heading hidden</h1>
+    <div class="wraper">
+        <div class="flex items-center justify-between">
+
+            <div id="dl-menu" class="dl-menuwrapper">
+                <button class="dl-trigger">Open Menu</button>
+                <ul class="dl-menu">
+                    <li>
+                        <a href="index.html">Home</a>
+                        </li>
+
+                    <li>
+                        <a href="portfolio-webDevelopment.php">Portfolio</a>
+                        <ul class="dl-submenu">
+                            <li>
+                                <a href="#">Web Development</a>
+                                <ul class="dl-submenu">
+                                    <li>
+                                        <a href="portfolio-webProjectAround.php">Around The World</a>
+                                    </li>
+                                    <li>
+                                        <a href="portfolio-homeland.php">Homeland</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            </ul>
+                    </li>
+
+                    <li>
+                        <a href="contact.php">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="w-[195px] md:w-[280px] sm:w-[200px] col:w-[50px]">
+                <a class="text-[45px] col:text-[25px] font-bold flex items-center text-white" href="index.php">
+                    <img
+                        style="max-height:60px"
+                        src="assets/images/logo_white_50.png"
+                        alt="logo"
+                    />
+                </a>
+            </div>
+            <ul class="md:hidden">
+                <li class="relative inline-block group">
+                    <a
+                        href="index.php"
+                        class="relative text-[16px] lg:text-[14px] py-[35px] xl:py-[30px] px-[20px] xl:px-[6px] text-white block capitalize font-base-font font-medium before:absolute before:left-0 before:bottom-0 before:w-full before:h-[4px] before:bg-[#59C378] before:content before:opacity-0 before:invisible before:transition-all before:rounded-[3px]"
+                        >Home</a
+                    >
+                </li>
+
+                <li class="relative inline-block group">
+                    <a
+                        href="portfolio-webDevelopment.php"
+                        class="relative group text-[16px] lg:text-[14px] py-[35px] xl:py-[30px] px-[20px] xl:px-[6px] text-white block capitalize font-base-font font-medium before:absolute before:left-0 before:bottom-0 before:w-full before:h-[4px] before:bg-[#59C378] before:content before:opacity-1 before:visible before:transition-all before:rounded-[3px] hover:before:opacity-100 hover:before:visible"
+                        >Portfolio</a
+                    >
+                    <ul
+                        class="absolute w-[240px] left-0 top-[110%] p-[20px] z-[111] bg-[#fff] shadow-[#3e419f17] transition-all opacity-0 invisible group-hover:opacity-100 group-hover:top-full group-hover:visible"
+                    >
+                        <li>
+                            <a
+                                href="portfolio-webDevelopment.php"
+                                class="text-[16px] lg:text-[14px] inline-block py-[5px] capitalize text-[#002642] group relative overflow-hidden font-bold transition-all after:absolute after:left-0 after:bottom-[5px] after:w-0 after:h-[1px] after:content after:bg-[#59C378] after:transition-all hover:after:w-full"
+                                >Web Development
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="relative inline-block">
+                    <a
+                        href="contact.php"
+                        class="relative group text-[16px] lg:text-[14px] py-[35px] xl:py-[30px] px-[20px] xl:px-[6px] text-white block capitalize font-base-font font-medium before:absolute before:left-0 before:bottom-0 before:w-full before:h-[4px] before:bg-[#59C378] before:content before:opacity-0 before:invisible before:transition-all before:rounded-[3px] hover:before:opacity-100 hover:before:visible"
+                        >Contact</a
+                    >
+                </li>
+            </ul>
+            <a
+                class="theme-btn col:w-50px"
+                download="Dēivid Sans' Resume"
+                href="assets/images/Dēivid_Sans_Resume.jpg"
+            >
+                <img
+                    class="hidden overflow-hidden opacity-0"
+                    alt="ImageName"
+                    href="assets/images/Dēivid_Sans_Resume.jpg"
+                />
+                Resume
+            </a>
+            </div>
+    </div>
+</section>
